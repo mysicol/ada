a32e3dbd8a0077739a9e79e07ac134de67e0cf1b
Abby <117459906+mysicol@users.noreply.github.com>
2024-02-24 11:14:42 -0500

    Added navigation button

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Main = () => {
+const Home = () => {
+  const navigation = useNavigation();
+
+  const homeNextPress = () => {
+    navigation.navigate('Who Was Ada?');
+  };
+
   return (
     <View
     style={styles.container}>
       <Text style={styles.title}>ada lovelace</Text>
       <Image style={styles.image} source={require('./assets/ada_lovelace.jpg')} />
+      <Button title="Continue to 'Who was Ada?'" onPress={homeNextPress} />
     </View>
+  );
+};
+
+const AdaSummary = () => {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.title}>who was ada?</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false, // Set this to false to hide the header
+      }}>
+        <Stack.Screen name="Start" component={Home} />
+        <Stack.Screen name="Who Was Ada?" component={AdaSummary} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -30,4 +63,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default App;
