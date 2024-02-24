016ab1999f7b2cd8608788aae0980a9ec6bc01f3
Abby <117459906+mysicol@users.noreply.github.com>
2024-02-24 11:53:03 -0500

    added button styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -15,7 +15,9 @@ const Home = () => {
     style={styles.container}>
       <Text style={styles.title}>ada lovelace</Text>
       <Image style={styles.image} source={require('./assets/ada_lovelace.jpg')} />
-      <Button title="Continue to 'Who was Ada?'" onPress={homeNextPress} />
+      <Pressable style={styles.nextButton} onPress={homeNextPress}>
+        <Text style={styles.buttonText}>continue to "who was ada?" {'>'}</Text>
+      </Pressable>
     </View>
   );
 };
@@ -24,6 +26,9 @@ const AdaSummary = () => {
   return (
     <View style={styles.container}>
       <Text style ={styles.title}>who was ada?</Text>
+      <View style = {styles.bodyContainer}>
+        <Text style ={styles.body}> testing testing testing</Text>
+      </View>
     </View>
   );
 };
@@ -48,11 +53,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: '#2bb8bd',
+  },
+  bodyContainer: {
+    width: 600,
+    height: 450,
+    padding: 30,
+    alignItems: 'left',
     backgroundColor: '#f7f6df',
   },
   title: {
     fontFamily: 'monospace',
     fontWeight: 'bold',
+    color: '#f7f6df',
     fontSize: 50,
     padding: 20
   },
@@ -61,6 +74,15 @@ const styles = StyleSheet.create({
     height: 300,
     resizeMode: 'stretch',
   },
+  nextButton: {
+    flex: 1,
+    alignItems: 'center',
+    margin: 10,
+  },
+  buttonText: {
+    color: '#f7f6df',
+    fontSize: 20
+  }
 });
 
 export default App;
